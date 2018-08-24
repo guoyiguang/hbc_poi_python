aa3bc8f7cf9e74330e3eb1ed04eb3476bace2031
yiguangguo <827267162@qq.com>
2018-08-24 14:09:52 +0800

    c

--- grap_pics.py
+++ grap_pics.py
@@ -143,7 +143,7 @@ def select_mo():
         sql = '''update cm_sight_mafengwo_etl set hbc_pics = '%s' where sight_id = %s'''%(img_str,sight_id)
         cursor.execute(sql)
         conn.commit()
-    # 关闭游标 释放链接
+    # 关闭游标 释放链
     conn.close()
     cursor.close()
 
